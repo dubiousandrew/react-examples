f95fb7b63cb39782787feed1a8525f3d7f851ccb
Andrew Harrison <andrew.harrison@semanticbits.com>
2020-07-02 14:00:37 -0400

    clean up function

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -5,11 +5,7 @@ import { observer } from "mobx-react";
 export const TodoItem = observer((props: { todo: Todo }) => {
   const style = props.todo.completed ? { textDecoration: "line-through" } : {};
   return (
-    <li
-      style={style}
-      onClick={() => props.todo.toggleTodo()}
-      key={props.todo.id}
-    >
+    <li style={style} onClick={props.todo.toggleTodo} key={props.todo.id}>
       {props.todo.content}
     </li>
   );
