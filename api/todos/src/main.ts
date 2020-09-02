668ba567f81aa1af308237f4532374d165d50ae8
Andrew Harrison <andrew.harrison@semanticbits.com>
2020-09-02 16:42:11 -0400

    add api services

--- api/todos/src/main.ts
+++ api/todos/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Todo Example')
+    .setDescription('The todo api description')
+    .setVersion('1')
+    .addTag('todos')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(8080);
 }
 bootstrap();
